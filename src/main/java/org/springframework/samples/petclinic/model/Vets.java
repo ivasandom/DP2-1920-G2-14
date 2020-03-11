9ab108c9bcbefe20a58dfd86e43860131288c80d
Iván <ivansd.99@gmail.com>
2020-03-11 18:27:39 +0100

    New repository Updated

--- src/main/java/org/springframework/samples/petclinic/model/Vets.java
+++ src/main/java/org/springframework/samples/petclinic/model/Vets.java
@@ -25,7 +25,7 @@ import javax.xml.bind.annotation.XmlRootElement;
  * Simple domain object representing a list of veterinarians. Mostly here to be used for
  * the 'vets' {@link org.springframework.web.servlet.view.xml.MarshallingView}.
  *
- * @author Arjen Poutsma
+ * @author Arjen PoutsmaA
  */
 @XmlRootElement
 public class Vets {
