d976c8e5bdcefe299fd60d32d065912ca0d257db
josgamdia1 <josgamdia1@alum.us.es>
2020-03-15 21:14:23 +0100

    Create appointment controller, repository and service.

---
+++ src/main/java/org/springframework/samples/petclinic/service/AppointmentService.java
@@ -0,0 +1,30 @@
+
+package org.springframework.samples.petclinic.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.samples.petclinic.model.Appointment;
+import org.springframework.samples.petclinic.repository.AppointmentRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class AppointmentService {
+
+	@Autowired
+	private AppointmentRepository appointmentRepository;
+	
+	@Autowired
+	public AppointmentService(AppointmentRepository appointmentRepository) {
+		this.appointmentRepository = appointmentRepository;
+	}
+	
+	@Transactional(readOnly = true)
+	public Iterable<Appointment> listAppointments() throws DataAccessException {
+		return appointmentRepository.findAll();
+	}
+	@Transactional
+	public void saveAppointment(Appointment appointment) throws DataAccessException {
+		appointmentRepository.save(appointment);
+	}		
+}
---
+++ src/main/java/org/springframework/samples/petclinic/web/AppointmentController.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2002-2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.samples.petclinic.web;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.samples.petclinic.model.Appointment;
+import org.springframework.samples.petclinic.service.AppointmentService;
+import org.springframework.samples.petclinic.service.AuthoritiesService;
+import org.springframework.samples.petclinic.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("appointments")
+public class AppointmentController {
+
+	private final AppointmentService appointmentService;
+
+	@Autowired
+	public AppointmentController(AppointmentService appointmentService, UserService userService, AuthoritiesService authoritiesService) {
+		this.appointmentService = appointmentService;
+	}
+
+	@InitBinder
+	public void setAllowedFields(WebDataBinder dataBinder) {
+		dataBinder.setDisallowedFields("id");
+	}
+	
+	@GetMapping()
+	public String listAppointments(Map<String, Object> model) {
+		Iterable<Appointment> appointments = this.appointmentService.listAppointments();
+		model.put("appointments", appointments);
+		return "appointments/list";
+	}
+
+}
