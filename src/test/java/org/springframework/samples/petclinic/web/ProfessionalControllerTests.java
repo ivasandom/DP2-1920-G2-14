e35ee2e8de94cc04b447c102cb641320a3ba94db
Alberto <murillojaenesalberto@gmail.com>
2020-03-30 11:43:44 +0200

    ProfessionalControllerTests

--- src/test/java/org/springframework/samples/petclinic/web/ProfessionalControllerTests.java
+++ src/test/java/org/springframework/samples/petclinic/web/ProfessionalControllerTests.java
@@ -1,13 +1,15 @@
 package org.springframework.samples.petclinic.web;
 
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 import java.util.Date;
 
+import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.BDDMockito;
@@ -20,12 +22,13 @@ import org.springframework.samples.petclinic.configuration.SecurityConfiguration
 import org.springframework.samples.petclinic.model.Appointment;
 import org.springframework.samples.petclinic.model.Client;
 import org.springframework.samples.petclinic.model.DocumentType;
+import org.springframework.samples.petclinic.model.Owner;
 import org.springframework.samples.petclinic.model.Professional;
+import org.springframework.samples.petclinic.model.Specialty;
 import org.springframework.samples.petclinic.service.AppointmentService;
 import org.springframework.samples.petclinic.service.AuthoritiesService;
 import org.springframework.samples.petclinic.service.ClientService;
 import org.springframework.samples.petclinic.service.ProfessionalService;
-import org.springframework.samples.petclinic.service.SpecialtyService;
 import org.springframework.samples.petclinic.service.UserService;
 import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.test.context.support.WithMockUser;
@@ -33,7 +36,6 @@ import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequ
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-
 
 @WebMvcTest(controllers = ProfessionalController.class, excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = WebSecurityConfigurer.class), excludeAutoConfiguration = SecurityConfiguration.class)
 class ProfessionalControllerTests {
@@ -57,9 +59,6 @@ class ProfessionalControllerTests {
 	private AppointmentService	appointmentService;
 
 	@MockBean
-	private SpecialtyService	specialtyService;
-	
-	@MockBean
 	private AuthoritiesService	authoritiesService;
 
 	@Autowired
@@ -76,53 +75,32 @@ class ProfessionalControllerTests {
 		this.pepe.setFirstName("Pepe");
 		this.pepe.setLastName("Gotera");
 		this.pepe.setEmail("pepegotera@gmail.com");
-		Date birthdate = new Date(1955-12-01);
+		Date birthdate = new Date(1955 - 12 - 01);
 		this.pepe.setBirthDate(birthdate);
-		Date registrationDate = new Date(2015-10-02);
+		Date registrationDate = new Date(2015 - 10 - 02);
 		this.pepe.setRegistrationDate(registrationDate);
 		this.pepe.setDocument("10203040T");
 		this.pepe.setDocumentType(DocumentType.nif);
+		Specialty sp = new Specialty();
+		sp.setId(ProfessionalControllerTests.TEST_SPECIALTY_ID);
+		sp.setName("Especial");
+		this.pepe.setSpecialty(sp);
 		BDDMockito.given(this.clinicService.findProfessionalById(ProfessionalControllerTests.TEST_PROFESSIONAL_ID)).willReturn(this.pepe);
+		BDDMockito.given(this.appointmentService.findAppointmentById(ProfessionalControllerTests.TEST_PROFESSIONAL_ID)).willReturn(new Appointment());
 	}
 
 	@WithMockUser(value = "spring")
-	@Test
-	void testInitCreationForm() throws Exception {
-		this.mockMvc.perform(MockMvcRequestBuilders.get("/clients/new")).andExpect(MockMvcResultMatchers.status().isOk()).andExpect(MockMvcResultMatchers.model().attributeExists("client"))
-			.andExpect(MockMvcResultMatchers.view().name("users/createClientForm"));
+    @Test
+    void testInitFindForm() throws Exception {
+		mockMvc.perform(get("/professionals/find")).andExpect(status().isOk()).andExpect(model().attributeExists("professional"))
+			.andExpect(view().name("professionals/find"));
 	}
 
 	@WithMockUser(value = "spring")
-	@Test
-	void testProcessCreationFormSuccess() throws Exception {
-		this.mockMvc.perform(MockMvcRequestBuilders.post("/clients/new")
-			.param("firstName", "Pepe")
-			.param("lastName", "Gotera").with(SecurityMockMvcRequestPostProcessors.csrf())
-			.param("email", "pepegotera@gmail.com")
-			.param("birthdate", "1955-12-4")
-			.param("registrationDate", "2015-07-23")
-			.param("document", "10203040T")
-			.param("documentType", "nif")
-			.param("healthInsurance", "Mafre")
-			.param("healthCardNumber", "1234567890")).andExpect(status().is2xxSuccessful());
-	}
+    @Test
+    void testProcessFindFormSuccess() throws Exception {
+		given(this.clinicService.findProByUsername("")).willReturn(pepe, new Professional());
 
-	@WithMockUser(value = "spring")
-	@Test
-	void testProcessCreationFormHasErrors() throws Exception {
-		mockMvc.perform(post("/clients/new")
-							.with(csrf())
-							.param("firstName", "George")
-							.param("lastName", "Franklin")
-							.param("registrationDate", "2015-07-23")
-							.param("document", "10223040T"))
-				.andExpect(status().isOk())
-				.andExpect(model().attributeHasErrors("client"))
-				.andExpect(model().attributeHasFieldErrors("client", "email"))
-				.andExpect(model().attributeHasFieldErrors("client", "birthdate"))
-				.andExpect(model().attributeHasFieldErrors("client", "registrationDate"))
-				.andExpect(model().attributeHasFieldErrors("client", "documentType"))
-				.andExpect(model().attributeHasFieldErrors("client", "healthInsurance"))
-				.andExpect(view().name("users/createClientForm"));
-	}
+		mockMvc.perform(get("/professionals")).andExpect(status().isOk()).andExpect(view().name("professionals/list"));
+}
 }
