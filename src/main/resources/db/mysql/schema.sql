2c8ded9872de89c55a277646fba74b7d6b0fc1d4
Iván Santos Domínguez <ivansd.99@gmail.com>
2020-05-12 00:43:25 +0200

    Update MySQL schema

--- src/main/resources/db/mysql/schema.sql
+++ src/main/resources/db/mysql/schema.sql
@@ -1,55 +1,160 @@
-CREATE TABLE IF NOT EXISTS vets (
-  id INT(4) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(30),
-  last_name VARCHAR(30),
-  INDEX(last_name)
+create table appointments (
+    id integer not null auto_increment,
+    date date,
+    reason varchar(255),
+    start_time time,
+    status varchar(32) default 'PENDING',
+    FOREIGN KEY (center_id) REFERENCES centers(id),
+    FOREIGN KEY (client_id) REFEREBCES clients(id),
+    FOREIGN KEY (diagnosis_id) REFERENCES diagnosis(id),
+    FOREIGN KEY (professional_id) REFERENCES professionals(id),
+    FOREIGN KEY (receipt_id) REFERENCES receipts(id),
+    FOREIGN KEY (specialty_id) REFERENCES specialties(id),
+    FOREIGN KEY (type_id) REFERENCES types(id),
+    primary key (id)
 ) engine=InnoDB;
 
-CREATE TABLE IF NOT EXISTS specialties (
-  id INT(4) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(80),
-  INDEX(name)
+create table authorities (
+    username varchar(255) not null,
+    authority varchar(255),
+    primary key (username)
 ) engine=InnoDB;
 
-CREATE TABLE IF NOT EXISTS vet_specialties (
-  vet_id INT(4) UNSIGNED NOT NULL,
-  specialty_id INT(4) UNSIGNED NOT NULL,
-  FOREIGN KEY (vet_id) REFERENCES vets(id),
-  FOREIGN KEY (specialty_id) REFERENCES specialties(id),
-  UNIQUE (vet_id,specialty_id)
+create table bills (
+    id integer not null auto_increment,
+    name varchar(200),
+    document varchar(255),
+    final_price double precision not null,
+    iva double precision not null,
+    last_name varchar(255),
+    price double precision not null,
+    type_document varchar(255),
+    FOREIGN KEY (receipt_id) REFERENCES receipts(id),
+    primary key (id)
 ) engine=InnoDB;
 
-CREATE TABLE IF NOT EXISTS types (
-  id INT(4) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(80),
-  INDEX(name)
+create table centers (
+    id integer not null auto_increment,
+    name varchar(200),
+    address varchar(255),
+    primary key (id)
 ) engine=InnoDB;
 
-CREATE TABLE IF NOT EXISTS owners (
-  id INT(4) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(30),
-  last_name VARCHAR(30),
-  address VARCHAR(255),
-  city VARCHAR(80),
-  telephone VARCHAR(20),
-  INDEX(last_name)
+create table clients (
+    id integer not null auto_increment,
+    birth_date datetime,
+    document varchar(255),
+    document_type integer not null,
+    email varchar(255),
+    first_name varchar(255),
+    last_name varchar(255),
+    registration_date datetime,
+    health_card_number varchar(255),
+    health_insurance varchar(255),
+    stripe_id varchar(255),
+    FOREIGN KEY (username) REFERENCES users(username),
+    primary key (id)
 ) engine=InnoDB;
 
-CREATE TABLE IF NOT EXISTS pets (
-  id INT(4) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(30),
-  birth_date DATE,
-  type_id INT(4) UNSIGNED NOT NULL,
-  owner_id INT(4) UNSIGNED NOT NULL,
-  INDEX(name),
-  FOREIGN KEY (owner_id) REFERENCES owners(id),
-  FOREIGN KEY (type_id) REFERENCES types(id)
+create table deseases (
+    id integer not null auto_increment,
+    name varchar(200),
+    primary key (id)
 ) engine=InnoDB;
 
-CREATE TABLE IF NOT EXISTS visits (
-  id INT(4) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  pet_id INT(4) UNSIGNED NOT NULL,
-  visit_date DATE,
-  description VARCHAR(255),
-  FOREIGN KEY (pet_id) REFERENCES pets(id)
+create table diagnosis (
+    id integer not null auto_increment,
+    date date,
+    description varchar(255),
+    primary key (id)
+) engine=InnoDB;
+
+create table diagnosis_deseases (
+    FOREIGN KEY (diagnosis_id) REFERENCES diagnosis(id),
+    FOREIGN KEY (desease_id) REFERENCES deseases(id),
+    primary key (diagnosis_id, desease_id)
+) engine=InnoDB;
+
+create table diagnosis_medicines (
+    FOREIGN KEY (diagnosis_id) REFERENCES diagnosis(id),
+    FOREIGN KEY (medicine_id) REFERENCES medicines(id),
+) engine=InnoDB;
+
+create table medicines (
+    id integer not null auto_increment,
+    name varchar(200),
+    price double precision not null,
+    primary key (id)
+) engine=InnoDB;
+
+create table payment_methods (
+    id integer not null auto_increment,
+    brand varchar(255),
+    token varchar(255),
+    FOREIGN KEY (client_id) REFERENCES clients(id),
+    primary key (id)
+) engine=InnoDB;
+
+create table professionals (
+    id integer not null auto_increment,
+    birth_date datetime,
+    document varchar(255),
+    document_type integer not null,
+    email varchar(255),
+    first_name varchar(255),
+    last_name varchar(255),
+    registration_date datetime,
+    collegiate_number varchar(255),
+    FOREIGN KEY (center_id) REFERENCES centers(id),
+    FOREIGN KEY (specialty_id) REFERENCES specialties(id),
+    FOREIGN KEY (username) REFERENCE users(username),
+    primary key (id)
+) engine=InnoDB;
+
+create table receipts (
+    id integer not null auto_increment,
+    price double precision not null,
+    status integer,
+    FOREIGN KEY (appointment_id) REFERENCES appointments(id),
+    primary key (id)
+) engine=InnoDB;
+
+create table schedule (
+    id integer not null auto_increment,
+    date date,
+    end_time time,
+    start_time time,
+    FOREIGN KEY (center_id) REFERENCES centers(id),
+    FOREIGN KEY (professional_id) REFERENCES professionals(id),
+    primary key (id)
+) engine=InnoDB;
+
+create table specialties (
+    id integer not null auto_increment,
+    name varchar(200),
+    primary key (id)
+) engine=InnoDB;
+
+create table transactions (
+    id integer not null auto_increment,
+    amount double precision not null,
+    status varchar(255),
+    success bit,
+    token varchar(255),
+    type integer,
+    FOREIGN KEY (receipt_id) REFERENCES receipts(id),
+    primary key (id)
+) engine=InnoDB;
+
+create table types (
+    id integer not null auto_increment,
+    name varchar(200),
+    primary key (id)
+) engine=InnoDB;
+
+create table users (
+    username varchar(255) not null,
+    enabled bit not null,
+    password varchar(60),
+    primary key (username)
 ) engine=InnoDB;
