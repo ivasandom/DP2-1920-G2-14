559da5263e648dd7409f5574c12cc74d5d410c52
Alberto <murillojaenesalberto@gmail.com>
2020-06-02 12:47:31 +0200

    Fix delete

--- src/main/java/org/springframework/samples/petclinic/model/Specialty.java
+++ src/main/java/org/springframework/samples/petclinic/model/Specialty.java
@@ -1,12 +1,7 @@
 
 package org.springframework.samples.petclinic.model;
 
-import java.util.Set;
-
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import lombok.Getter;
@@ -18,9 +13,4 @@ import lombok.Setter;
 @Table(name = "specialties")
 public class Specialty extends NamedEntity {
 
-	@OneToMany(cascade = CascadeType.ALL, mappedBy = "specialty", fetch = FetchType.EAGER)
-	private Set<Appointment>	appointments;
-
-	@OneToMany(cascade = CascadeType.ALL, mappedBy = "specialty", fetch = FetchType.EAGER)
-	private Set<Professional>	professionals;
 }
